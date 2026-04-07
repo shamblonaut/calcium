f0e1f279d67e4d6a236ace749af981d0a35e6154
Shaheem Niyaz <shaheembn@gmail.com>
2026-04-07 16:17:57 +0530

    Implement clear button

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let digits = document.querySelectorAll(".btn-num");
 let result = document.querySelector(".result");
+let clear = document.getElementById("btn-clear");
 
 digits.forEach((digit) => {
   digit.addEventListener("click", (event) => {
@@ -9,4 +10,8 @@ digits.forEach((digit) => {
 
     result.textContent += digit.textContent;
   });
+});
+
+clear.addEventListener("click", (event) => {
+  result.textContent = "0";
 });
